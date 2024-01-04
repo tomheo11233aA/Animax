b3778139b57a720b0c1435a24218ff7a494e4019
faIIinlove <phucnamvan@gmail.com>
2024-01-04 21:07:44 +0700

    Add new font files

---
+++ src/themes/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+    light: {
+        black: 'black',
+    },
+    dark: {
+        black: 'white',
+    },
+    mainColor: '#06C149',
+    shadowColor: '#06c14940',
+    black: '#212121',
+    black2: '#424242',
+    gray: '#616161'
+}
