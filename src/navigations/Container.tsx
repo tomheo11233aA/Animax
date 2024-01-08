2ecf925765b138491b57b8c653c0f3ce0d93bb61
faIIinlove <phucnamvan@gmail.com>
2024-01-08 21:02:43 +0700

    Fix navigation issue in Hello screen

--- src/navigations/Container.tsx
+++ src/navigations/Container.tsx
@@ -4,13 +4,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { navigationRef } from '@utils/navigationRef'
 //screens
 import Hello from "@screens/Hello";
-<<<<<<< HEAD
 import Started from "@screens/Users/Started";
 import Signinsocial from "@screens/Users/Signinsocial";
 import Signup from "@screens/Users/Signup";
-=======
 import Main from "./Main";
->>>>>>> 5b6adb80f7e25f20c407b0885413eccd2d011f09
 
 const Stack = createNativeStackNavigator()
 const Container = () => {
@@ -18,13 +15,10 @@ const Container = () => {
         <NavigationContainer ref={navigationRef}>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name={screens.HELLO} component={Hello} />
-<<<<<<< HEAD
                 <Stack.Screen name={screens.STARTED} component={Started} />
                 <Stack.Screen name={screens.SIGNUP} component={Signup} />
                 <Stack.Screen name={screens.SIGNINSOCIAL} component={Signinsocial} />
-=======
                 <Stack.Screen name={screens.MAIN} component={Main} />
->>>>>>> 5b6adb80f7e25f20c407b0885413eccd2d011f09
             </Stack.Navigator>
         </NavigationContainer>
     )
