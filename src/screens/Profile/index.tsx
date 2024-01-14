b40f6db5a873782a7b4584ea17ed832037a7144b
faIIinlove <phucnamvan@gmail.com>
2024-01-14 15:00:10 +0700

    abc

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useAppDispatch } from '@hooks/redux'
+import { setLogin } from '@redux/slice/userSlice'
+import { AppDispatch } from '@redux/store/store'
+import { TouchableOpacity } from 'react-native'
 
 const Profile = () => {
+  const dispatch: AppDispatch = useAppDispatch()
   return (
     <View>
       <Text>Profile</Text>
+      <TouchableOpacity onPress={() => {
+        dispatch(setLogin(false))
+      }
+      }>
+        <Text style={{color: 'black'}}>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
